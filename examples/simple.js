b492921942fd436cff130d7be75049426f56bcd6
yiminghe <yiminghe@gmail.com>
2015-07-16 18:22:05 +0800

    Updates

---
+++ examples/simple.js
@@ -0,0 +1,10 @@
+webpackJsonp([0,1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"./examples/simple.js\""); }());
+
+
+/***/ }
+]);
+//# sourceMappingURL=simple.js.map

c3d716b39006f9a74bd6e8a72bc094fd0242bb3b
yiminghe <yiminghe@gmail.com>
2017-05-26 13:40:36 +0800

    Updates

--- examples/fail.js
+++ examples/fail.js
@@ -1,60 +1,62 @@
-webpackJsonp([0],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+webpackJsonp([1],{
 
-	module.exports = __webpack_require__(1);
+/***/ 193:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(86);
 
 
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ }),
 
-	'use strict';
-	
-	var React = __webpack_require__(2);
-	var domAlign = __webpack_require__(33);
-	var ReactDOM = __webpack_require__(43);
-	
-	var Test = React.createClass({
-	  displayName: 'Test',
-	  align: function align() {
-	    var ret = domAlign(this.refs.source, this.refs.target, {
-	      points: ['tl', 'tl'],
-	      overflow: {
-	        adjustY: 1
-	      }
-	    });
-	    console.log(ret);
-	  },
-	  render: function render() {
-	    return React.createElement(
-	      'div',
-	      { style: { height: 1000 } },
-	      React.createElement(
-	        'button',
-	        { ref: 'target' },
-	        'target'
-	      ),
-	      React.createElement('div', { style: { height: 100 } }),
-	      React.createElement(
-	        'button',
-	        { onClick: this.align },
-	        'align'
-	      ),
-	      React.createElement(
-	        'div',
-	        {
-	          ref: 'source',
-	          style: { position: 'absolute', width: 100, height: 100, border: '1px solid red' }
-	        },
-	        'oo'
-	      )
-	    );
-	  }
-	});
-	
-	ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
+/***/ 86:
+/***/ (function(module, exports, __webpack_require__) {
 
-/***/ }
-]);
+var React = __webpack_require__(85);
+var domAlign = __webpack_require__(51);
+var ReactDOM = __webpack_require__(59);
+
+var Test = React.createClass({
+  displayName: 'Test',
+  align: function align() {
+    var ret = domAlign(this.refs.source, this.refs.target, {
+      points: ['bl', 'bl'],
+      overflow: {
+        adjustY: 1
+      }
+    });
+    console.log(ret);
+  },
+  render: function render() {
+    window.align = this.align;
+    return React.createElement(
+      'div',
+      { style: { height: 1000 } },
+      React.createElement(
+        'button',
+        { ref: 'target' },
+        'target'
+      ),
+      React.createElement('div', { style: { height: 100 } }),
+      React.createElement(
+        'button',
+        { onClick: this.align },
+        'align'
+      ),
+      React.createElement(
+        'div',
+        {
+          ref: 'source',
+          style: { position: 'absolute', width: 100, height: 200, border: '1px solid red' }
+        },
+        'oo'
+      )
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(Test, null), document.getElementById('__react-content'));
+
+/***/ })
+
+},[193]);
 //# sourceMappingURL=fail.js.map
--- examples/simple.js
+++ examples/simple.js
@@ -1,234 +1,232 @@
-webpackJsonp([1],{
+webpackJsonp([0],{
 
-/***/ 0:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 194:
+/***/ (function(module, exports, __webpack_require__) {
 
-	module.exports = __webpack_require__(189);
+module.exports = __webpack_require__(87);
 
 
-/***/ },
+/***/ }),
 
-/***/ 189:
-/***/ function(module, exports, __webpack_require__) {
+/***/ 87:
+/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
-	
-	var React = __webpack_require__(2);
-	var ReactDOM = __webpack_require__(43);
-	
-	function $id(id) {
-	  return document.getElementById(id);
-	}
-	
-	function $val(sel) {
-	  sel = $id(sel);
-	  return sel.value;
-	}
-	
-	var domAlign = __webpack_require__(33);
-	
-	function align() {
-	  domAlign($id('source'), $id('target'), {
-	    points: [$val('source_align_tb') + $val('source_align_lr'), $val('target_align_tb') + $val('target_align_lr')],
-	    offset: [$val('offset1'), $val('offset2')],
-	    targetOffset: [$val('targetOffset1'), $val('targetOffset2')],
-	    overflow: {
-	      adjustX: $id('adjustX').checked,
-	      adjustY: $id('adjustY').checked
-	    },
-	    useCssRight: $id('useCssRight').checked,
-	    useCssBottom: $id('useCssBottom').checked,
-	    useCssTransform: $id('useCssTransform').checked
-	  });
-	}
-	
-	var div = React.createElement(
-	  'div',
-	  null,
-	  React.createElement(
-	    'h1',
-	    null,
-	    'dom-align'
-	  ),
-	  React.createElement(
-	    'div',
-	    null,
-	    'source:',
-	    React.createElement(
-	      'select',
-	      { id: 'source_align_tb' },
-	      React.createElement(
-	        'option',
-	        { value: 't' },
-	        't'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'c' },
-	        'c'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'b' },
-	        'b'
-	      )
-	    ),
-	    React.createElement(
-	      'select',
-	      { id: 'source_align_lr' },
-	      React.createElement(
-	        'option',
-	        { value: 'l' },
-	        'l'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'c' },
-	        'c'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'r' },
-	        'r'
-	      )
-	    ),
-	    '\xA0 target:',
-	    React.createElement(
-	      'select',
-	      { id: 'target_align_tb' },
-	      React.createElement(
-	        'option',
-	        { value: 't' },
-	        't'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'c' },
-	        'c'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'b' },
-	        'b'
-	      )
-	    ),
-	    React.createElement(
-	      'select',
-	      { id: 'target_align_lr' },
-	      React.createElement(
-	        'option',
-	        { value: 'l' },
-	        'l'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'c' },
-	        'c'
-	      ),
-	      React.createElement(
-	        'option',
-	        { value: 'r' },
-	        'r'
-	      )
-	    ),
-	    '\xA0 offset: [',
-	    React.createElement('input', { type: 'offset', id: 'offset1', defaultValue: '0', size: '3' }),
-	    ',',
-	    React.createElement('input', { type: 'offset', id: 'offset2', defaultValue: '0', size: '3' }),
-	    '] \xA0 targetOffset: [',
-	    React.createElement('input', { type: 'offset', id: 'targetOffset1', defaultValue: '0', size: '3' }),
-	    ',',
-	    React.createElement('input', { type: 'offset', id: 'targetOffset2', defaultValue: '0', size: '3' }),
-	    '] \xA0 overflow: \xA0',
-	    React.createElement(
-	      'label',
-	      null,
-	      'adjustX:',
-	      React.createElement('input', { type: 'checkbox', id: 'adjustX' })
-	    ),
-	    '\xA0',
-	    React.createElement(
-	      'label',
-	      null,
-	      'adjustY:',
-	      React.createElement('input', { type: 'checkbox', id: 'adjustY' })
-	    ),
-	    '\xA0',
-	    React.createElement(
-	      'label',
-	      null,
-	      'useCssBottom:',
-	      React.createElement('input', { type: 'checkbox', id: 'useCssBottom' })
-	    ),
-	    '\xA0',
-	    React.createElement(
-	      'label',
-	      null,
-	      'useCssRight:',
-	      React.createElement('input', { type: 'checkbox', id: 'useCssRight' })
-	    ),
-	    '\xA0',
-	    React.createElement(
-	      'label',
-	      null,
-	      'useCssTransform:',
-	      React.createElement('input', { type: 'checkbox', id: 'useCssTransform', defaultChecked: !!window.TransitionEvent })
-	    ),
-	    '\xA0',
-	    React.createElement(
-	      'button',
-	      { id: 'align', onClick: align },
-	      'align'
-	    ),
-	    React.createElement('br', null),
-	    React.createElement(
-	      'div',
-	      {
-	        style: {
-	          width: 400,
-	          height: 400,
-	          overflow: 'auto',
-	          border: '1px solid green',
-	          position: 'relative'
-	        }
-	      },
-	      React.createElement(
-	        'div',
-	        {
-	          style: {
-	            background: 'yellow',
-	            width: 240,
-	            height: 240,
-	            margin: 50
-	          },
-	          id: 'target'
-	        },
-	        'target node'
-	      ),
-	      React.createElement('div', { style: { width: 1000, height: 1000 } }),
-	      React.createElement(
-	        'div',
-	        {
-	          style: {
-	            background: 'red',
-	            width: 50,
-	            height: 50,
-	            left: 0,
-	            top: 0,
-	            position: 'absolute',
-	            transition: 'all 0.5s'
-	          },
-	          id: 'source'
-	        },
-	        'source node'
-	      )
-	    )
-	  )
-	);
-	
-	ReactDOM.render(div, $id('__react-content'));
+var React = __webpack_require__(85);
+var ReactDOM = __webpack_require__(59);
 
-/***/ }
+function $id(id) {
+  return document.getElementById(id);
+}
 
-});
+function $val(sel) {
+  sel = $id(sel);
+  return sel.value;
+}
+
+var domAlign = __webpack_require__(51);
+
+function align() {
+  domAlign($id('source'), $id('target'), {
+    points: [$val('source_align_tb') + $val('source_align_lr'), $val('target_align_tb') + $val('target_align_lr')],
+    offset: [$val('offset1'), $val('offset2')],
+    targetOffset: [$val('targetOffset1'), $val('targetOffset2')],
+    overflow: {
+      adjustX: $id('adjustX').checked,
+      adjustY: $id('adjustY').checked
+    },
+    useCssRight: $id('useCssRight').checked,
+    useCssBottom: $id('useCssBottom').checked,
+    useCssTransform: $id('useCssTransform').checked
+  });
+}
+
+var div = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'dom-align'
+  ),
+  React.createElement(
+    'div',
+    null,
+    'source:',
+    React.createElement(
+      'select',
+      { id: 'source_align_tb' },
+      React.createElement(
+        'option',
+        { value: 't' },
+        't'
+      ),
+      React.createElement(
+        'option',
+        { value: 'c' },
+        'c'
+      ),
+      React.createElement(
+        'option',
+        { value: 'b' },
+        'b'
+      )
+    ),
+    React.createElement(
+      'select',
+      { id: 'source_align_lr' },
+      React.createElement(
+        'option',
+        { value: 'l' },
+        'l'
+      ),
+      React.createElement(
+        'option',
+        { value: 'c' },
+        'c'
+      ),
+      React.createElement(
+        'option',
+        { value: 'r' },
+        'r'
+      )
+    ),
+    '\xA0 target:',
+    React.createElement(
+      'select',
+      { id: 'target_align_tb' },
+      React.createElement(
+        'option',
+        { value: 't' },
+        't'
+      ),
+      React.createElement(
+        'option',
+        { value: 'c' },
+        'c'
+      ),
+      React.createElement(
+        'option',
+        { value: 'b' },
+        'b'
+      )
+    ),
+    React.createElement(
+      'select',
+      { id: 'target_align_lr' },
+      React.createElement(
+        'option',
+        { value: 'l' },
+        'l'
+      ),
+      React.createElement(
+        'option',
+        { value: 'c' },
+        'c'
+      ),
+      React.createElement(
+        'option',
+        { value: 'r' },
+        'r'
+      )
+    ),
+    '\xA0 offset: [',
+    React.createElement('input', { type: 'offset', id: 'offset1', defaultValue: '0', size: '3' }),
+    ',',
+    React.createElement('input', { type: 'offset', id: 'offset2', defaultValue: '0', size: '3' }),
+    '] \xA0 targetOffset: [',
+    React.createElement('input', { type: 'offset', id: 'targetOffset1', defaultValue: '0', size: '3' }),
+    ',',
+    React.createElement('input', { type: 'offset', id: 'targetOffset2', defaultValue: '0', size: '3' }),
+    '] \xA0 overflow: \xA0',
+    React.createElement(
+      'label',
+      null,
+      'adjustX:',
+      React.createElement('input', { type: 'checkbox', id: 'adjustX' })
+    ),
+    '\xA0',
+    React.createElement(
+      'label',
+      null,
+      'adjustY:',
+      React.createElement('input', { type: 'checkbox', id: 'adjustY' })
+    ),
+    '\xA0',
+    React.createElement(
+      'label',
+      null,
+      'useCssBottom:',
+      React.createElement('input', { type: 'checkbox', id: 'useCssBottom' })
+    ),
+    '\xA0',
+    React.createElement(
+      'label',
+      null,
+      'useCssRight:',
+      React.createElement('input', { type: 'checkbox', id: 'useCssRight' })
+    ),
+    '\xA0',
+    React.createElement(
+      'label',
+      null,
+      'useCssTransform:',
+      React.createElement('input', { type: 'checkbox', id: 'useCssTransform', defaultChecked: !!window.TransitionEvent })
+    ),
+    '\xA0',
+    React.createElement(
+      'button',
+      { id: 'align', onClick: align },
+      'align'
+    ),
+    React.createElement('br', null),
+    React.createElement(
+      'div',
+      {
+        style: {
+          width: 400,
+          height: 400,
+          overflow: 'auto',
+          border: '1px solid green',
+          position: 'relative'
+        }
+      },
+      React.createElement(
+        'div',
+        {
+          style: {
+            background: 'yellow',
+            width: 240,
+            height: 240,
+            margin: 50
+          },
+          id: 'target'
+        },
+        'target node'
+      ),
+      React.createElement('div', { style: { width: 1000, height: 1000 } }),
+      React.createElement(
+        'div',
+        {
+          style: {
+            background: 'red',
+            width: 50,
+            height: 50,
+            left: 0,
+            top: 0,
+            position: 'absolute',
+            transition: 'all 0.5s'
+          },
+          id: 'source'
+        },
+        'source node'
+      )
+    )
+  )
+);
+
+ReactDOM.render(div, $id('__react-content'));
+
+/***/ })
+
+},[194]);
 //# sourceMappingURL=simple.js.map

e195e3ea1aea0744f050d1ec4db3913838fe97f4
zombiej <smith3816@gmail.com>
2019-05-28 22:27:45 +0800

    round number

--- src/getElFuturePos.js
+++ src/getElFuturePos.js
@@ -6,8 +6,8 @@ function getElFuturePos(elRegion, refNodeRegion, points, offset, targetOffset) {
   const diff = [p2.left - p1.left, p2.top - p1.top];
 
   return {
-    left: elRegion.left - diff[0] + offset[0] - targetOffset[0],
-    top: elRegion.top - diff[1] + offset[1] - targetOffset[1],
+    left: Math.round(elRegion.left - diff[0] + offset[0] - targetOffset[0]),
+    top: Math.round(elRegion.top - diff[1] + offset[1] - targetOffset[1]),
   };
 }
 

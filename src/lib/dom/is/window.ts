56ec31b710a2a5200843bf8b0799bfb10b197405
Savchenko Alexander <asavchenko@datatech.ru>
2023-03-14 14:29:04 +0400

    add test for getPBMwidth

--- src/lib/dom/is/window.ts
+++ src/lib/dom/is/window.ts
@@ -5,9 +5,5 @@
 export function isWindow(input: unknown): input is Window {
   // must use == for ie8
   /* eslint eqeqeq:0 */
-  return (
-    input !== null &&
-    input !== undefined &&
-    input == (input as Record<string, unknown>).window
-  );
+  return input !== null && input !== undefined && input == (input as Record<string, unknown>)['window']
 }
